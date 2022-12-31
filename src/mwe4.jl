74a27496513dabf83f764c6d56437607ff426c30
Nicholas Klugman <13633349+nicholaskl97@users.noreply.github.com>
2022-12-31 17:43:51 +1300

    Removed temporary debug files

--- src/mwe4.jl
+++
@@ -1,9 +0,0 @@
-ex = :(u(x,0,y-5))
-indvars = ex.args[2:end]
-
-var_ = :($(Expr(:$, :u)))
-
-ex2 = :()
-ex2.head = ex.head
-#ex2.args = [var_, :( $indvars )] 
-ex2.args = [var_, :( [$(indvars...)] )]
